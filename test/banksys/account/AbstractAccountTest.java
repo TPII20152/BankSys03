ce202433341226fd43c26df732ed444f58efb86f
Ismalia Santiago <ismaliadulce@gmail.com>
2016-01-25 14:30:00 -0300

    Still trying to figure out Travis CI (#1)

---
+++ test/banksys/account/AbstractAccountTest.java
@@ -0,0 +1,34 @@
+package banksys.account;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AbstractAccountTest {
+
+	@Test
+	public void testAbstractAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCredit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDebit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBalance() {
+		fail("Not yet implemented");
+	}
+
+}
