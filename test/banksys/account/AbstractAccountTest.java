4c746522547e945bc33cbf6ac4dbd3b871f04a03
Ismália Santiago <ismaliadulce@gmail.com>
2016-01-29 20:06:03 -0300

    Testing AbstractAccount (close #8)

--- test/banksys/account/AbstractAccountTest.java
+++ test/banksys/account/AbstractAccountTest.java
@@ -4,31 +4,69 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import banksys.account.exception.InsufficientFundsException;
+import banksys.account.exception.NegativeAmountException;
+
 public class AbstractAccountTest {
 
 	@Test
 	public void testAbstractAccount() {
-		fail("Not yet implemented");
+		AbstractAccount account = new OrdinaryAccount("1234");
+		assertEquals("1234", account.getNumber());
 	}
 
 	@Test
-	public void testCredit() {
-		fail("Not yet implemented");
+	public void testPositiveCredit() throws NegativeAmountException {
+		AbstractAccount account = new OrdinaryAccount("1234");
+		double amount = 10.5;
+		account.credit(amount);
+		assertEquals(amount, account.getBalance(), 0);
+	}
+	
+	@Test (expected = NegativeAmountException.class)
+	public void testNegativeCredit() throws NegativeAmountException {
+		AbstractAccount account = new OrdinaryAccount("1234");
+		double amount = -10.5;
+		account.credit(amount);
+		assertEquals(0, account.getBalance(), 0);
 	}
 
 	@Test
-	public void testDebit() {
-		fail("Not yet implemented");
+	public void testPositiveDebit() throws NegativeAmountException, InsufficientFundsException {
+		AbstractAccount account = new OrdinaryAccount("1234");
+		double creditAmount = 10;
+		double debitAmount = 2;
+		account.credit(creditAmount);
+		account.debit(debitAmount);
+		assertEquals(8, account.getBalance(), 0);
+	}
+	
+	@Test (expected = NegativeAmountException.class)
+	public void testNegativeDebit() throws NegativeAmountException, InsufficientFundsException {
+		AbstractAccount account = new OrdinaryAccount("1234");
+		double amount = -2;
+		account.debit(amount);
+		assertEquals(0, account.getBalance(), 0);
+	}
+	
+	@Test (expected = InsufficientFundsException.class)
+	public void testNoFundsDebit() throws NegativeAmountException, InsufficientFundsException {
+		AbstractAccount account = new OrdinaryAccount("1234");
+		double amount = 10;
+		account.debit(amount);
+		assertEquals(0, account.getBalance(), 0);
 	}
 
 	@Test
 	public void testGetNumber() {
-		fail("Not yet implemented");
+		AbstractAccount account = new OrdinaryAccount("1234");
+		assertEquals("1234", account.getNumber());
 	}
 
 	@Test
 	public void testGetBalance() {
-		fail("Not yet implemented");
+		AbstractAccount account = new OrdinaryAccount("1234");
+		assertEquals(0, account.getBalance(), 0);
 	}
 
 }
